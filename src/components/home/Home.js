33163bcfc951c6d2cd3b03f1f2ac6ebe46e99e45
akashdevadiga <akashdevadiga260@gmail.com>
2024-03-27 02:13:14 +0530

    next image

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,9 @@
+import Image from 'next/image';
 import Data from './Data';
 import './home.css'
 import ScrollDown from './ScrollDown';
 import Social from './Social';
+import ProfileImg from "/public/assets/profile10.jpg";
 
 const Home = () => {
 
@@ -10,7 +12,10 @@ const Home = () => {
             <div className="home__container container grid">
                 <div className="home__content grid">
                     <Social />
-                    <div className="home__img"></div>
+                    {/* <div className="home__img"></div> */}
+                    <div className='profile__img'>
+                    <Image src={ProfileImg} alt="" className="" width={300} height={300} />
+                    </div>
                     <Data />
                 </div>
                 <ScrollDown />
